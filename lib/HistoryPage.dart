b0ebd26bd84e69ec7d3f0808bdfed58214598a4d
Delwinnn <221110202@students.mikroskil.ac.id>
2024-04-10 10:12:44 +0700

    menambahkan dan membuat fitur history

--- lib/HistoryPage.dart
+++ lib/HistoryPage.dart
@@ -1,37 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:program/component/data.dart';
 import 'package:provider/provider.dart';
 
 class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key});
+  String name;
+  HistoryPage({super.key,required this.name});
 
   @override
-  State<HistoryPage> createState() => _HistoryPageState();
+  _HistoryPageState createState() => _HistoryPageState();
 }
 
 class _HistoryPageState extends State<HistoryPage> {
-  List hasil = [];
-
-  @override
-  void initState() {
-    super.initState();
-    List allsales = Provider.of<ProviderGudang>(context,listen: false).Gudang.sales;
-    List allpurchase = Provider.of<ProviderGudang>(context,listen: false).Gudang.purchase;
-  }
+  List historys = [];
+  int found = 0;
 
   @override
   Widget build(BuildContext context) {
+    List sales = Provider.of<ProviderGudang>(context).Gudang.sales;
+    List purchase = Provider.of<ProviderGudang>(context).Gudang.purchase;
+    List historys = sales;
     return Scaffold(
       appBar: AppBar(
-        title: Text("History"),
+        title: Text('History Page'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey[350],
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
-            Text("${hasil}")
-          ]
-        ),
+            ...historys.map((e) {
+              for (int i = 0 ; i<e[4].length ; i++) {
+                if(e[4][i].contains("${widget.name}")) {
+                  found+=1;
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 7, vertical: 7),
+                    child: ListTile(
+                      title: Row(
+                        children: [
+                          Text("${e[1]}"),
+                          Spacer(),
+                          Text('${e[0]}'),
+                        ],
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Customer: ${e[2]}'),
+                          Row(
+                            mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Quantity: ${e[4][i][3]}'),
+                              Text('Price: ${
+                                NumberFormat.currency(
+                                  locale: 'id_ID',
+                                  symbol: '',
+                                  decimalDigits: 0,
+                                ).format(e[4][i][2])
+                              }'),
+                              ],
+                          ),
+                          Divider(
+                            color: Colors.black, 
+                            height: 16, 
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Total:'),
+                              Text(
+                                NumberFormat.currency(
+                                  locale: 'id_ID',
+                                  symbol: 'Rp. ',
+                                  decimalDigits: 2,
+                                ).format(e[4][i][3]*e[4][i][2]),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              }
+              return SizedBox();
+            }),
+            if (found==0)
+            Text("No History for this item") 
+          ],
+        )
       ),
     );
   }
--- lib/component/cardstock.dart
+++ lib/component/cardstock.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:program/HistoryPage.dart';
 
 class CardStock extends StatelessWidget {
   final dynamic produk;
@@ -8,40 +9,47 @@ class CardStock extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white
-      ),
-      padding: EdgeInsets.all(12),
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 5,vertical: 6),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image(
-            image: NetworkImage('${produk[0]}'),
-            height: MediaQuery.of(context).size.height/8,
-            width: 100,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:[
-                    Text("${produk[1]}",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),maxLines: 2,overflow: TextOverflow.ellipsis),
-                    Text("${produk[2]}",style: TextStyle(fontSize: 13,color: Colors.grey[600])),
-                  ]),
-                Text('Stock : ${produk[3]}',style: TextStyle(fontSize: 16)),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => HistoryPage(name: produk[1]),)
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white
+        ),
+        padding: EdgeInsets.all(12),
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.symmetric(horizontal: 5,vertical: 6),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: NetworkImage('${produk[0]}'),
+              height: MediaQuery.of(context).size.height/8,
+              width: 100,
+              fit: BoxFit.contain,
             ),
-          )
-        ],
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:[
+                      Text("${produk[1]}",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),maxLines: 2,overflow: TextOverflow.ellipsis),
+                      Text("${produk[2]}",style: TextStyle(fontSize: 13,color: Colors.grey[600])),
+                    ]),
+                  Text('Stock : ${produk[3]}',style: TextStyle(fontSize: 16)),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
